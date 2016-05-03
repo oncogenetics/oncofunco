44bc0ba3fe0abc0145fef61ac018bdfbdb5ee2fe
d4l997 <daniell@icr.ac.uk>
2016-05-03 18:06:08 +0100

    Add allMatch example

--- R/allMatch.R
+++ R/allMatch.R
@@ -1,7 +1,22 @@
 #' Match all elements in vector.
 #'
-#' Return all matching indexes in query vector in subject vector.
+#' Return all matching indexes in subject vector from query vector.
 #' @export allMatch
+#' @examples
+#' # Input query and subject vecots
+#' match.list <- allMatch(query = c(1,2,3), subject = c(1,1,2,2,3,3,2))
+#'
+#' # Output is query length list with subject indexes
+#' match.list
+#'
+#' # $`1`
+#' # [1] 1 2
+#' #
+#' # $`2`
+#' # [1] 3 4 7
+#' #
+#' # $`3`
+#' # [1] 5 6
 
 allMatch <- function(query, subject) {
 m1 <- match(query, subject)
@@ -17,7 +32,7 @@ while (sum(is.na(mx))!=length(mx)) {
   }
 }
 result.mat <- matrix(result, nrow = length(query))
-out <- lapply(1:dim(result.mat)[1], function(x) {result.mat[x,]})
+out <- lapply(1:nrow(result.mat), function(x) {result.mat[x,][! is.na(result.mat[x,])]})
 names(out) <- query
 return(out)
 }

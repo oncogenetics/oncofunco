9e01d70c43d423d575c334fc91b67b2b74179ac2
d4l997 <daniell@104780CGNLT.icr.ac.uk>
2016-05-05 10:34:28 +0100

    Clean up allMatch code and add comments

--- R/allMatch.R
+++ R/allMatch.R
@@ -1,6 +1,6 @@
 #' Match all elements in vector.
 #'
-#' Return all matching indexes in subject vector from query vector.
+#' Return all matching element indexes in subject vector from query vector.
 #' @export allMatch
 #' @examples
 #' # Input query and subject vecots
@@ -18,21 +18,34 @@
 #' # $`3`
 #' # [1] 5 6
 
-allMatch <- function(query, subject) {
-m1 <- match(query, subject)
-mx <- m1
-result <- m1
+allMatch <-
+  function(query, subject) {
+    # first match
+    m1 <- match(query, subject)
+    mx <- m1
+    # remove match
+    subject[mx] <- NA
+    # add to output
+    result <- m1
 
-while (sum(is.na(mx))!=length(mx)) {
-  subject[mx] <- NA
-  m2 <- match(query, subject)
-  mx <- m2
-  if(sum(is.na(mx))!=length(mx)) {
-    result <- c(result, m2)
+    # while there is a single match
+    while (sum(is.na(mx)) != length(mx)) {
+      m2 <- match(query, subject)
+      mx <- m2
+      subject[mx] <- NA
+      result <- c(result, m2)
+    }
+
+    # convert results to matrix
+    result.mat <- matrix(result, nrow = length(query))
+
+    # convert results to list
+    out <-
+      lapply(1:nrow(result.mat), function(x) {
+        result.mat[x, ][!is.na(result.mat[x, ])]
+      })
+    # add names
+    names(out) <- query
+    return(out)
   }
-}
-result.mat <- matrix(result, nrow = length(query))
-out <- lapply(1:nrow(result.mat), function(x) {result.mat[x,][! is.na(result.mat[x,])]})
-names(out) <- query
-return(out)
-}
+
